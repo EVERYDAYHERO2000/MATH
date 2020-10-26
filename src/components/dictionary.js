53df1a0dd6f8b3fb3b880ec496d6ac8dbb2bdcf9
Ilya Aralin <aralinilya@yandex-team.ru>
2020-10-26 23:55:00 +0300

    add test modul

---
+++ src/components/dictionary.js
@@ -0,0 +1,18 @@
+export default {
+    multiplication : {
+        multyplication : '×',
+        asterisk : '*',
+        dot : '⋅',
+    },
+    division : {
+        colon : '∶',
+        division : '÷',
+        slash : '∕'
+    },
+    exponentiation : {
+        algol : '↑',
+        basic : '^',
+        haskell : '^^',
+        ada : '**'
+    }
+}
