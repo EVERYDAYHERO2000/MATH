50942b32799b186551550179c065368636cb1509
Julia Belokrinitskaya <bel_julia@mail.ru>
2020-11-02 01:06:26 +0300

    Empty & Number

--- src/components/MathCanvas/MathCanvas.vue
+++ src/components/MathCanvas/MathCanvas.vue
@@ -1,15 +1,41 @@
 <template>
     <div class="math-canvas">
         y = f(x) = 2a + 3x π
+
+        <component :is="entity" v-bind:val="value"
+          v-on:isempty="entity='empty'"
+          v-on:isnumber="onIsNumber">
+        </component>
     </div>
 </template>
 
 <script>
+    import Empty from '../MathEntities/Empty/Empty.vue';
+    import Number from '../MathEntities/Number/Number.vue';
+
     export default {
         name: 'MathCanvas',
-        props: {
-        }    
-    }    
+        components: {
+            Empty,
+            Number
+        },
+        created: function () {
+          console.log('Refs parent', this.$refs);
+          // this.focus();
+        },
+        data: function() {
+            return {
+                entity: 'empty',
+                value: null
+            }
+        },
+        methods: {
+          onIsNumber: function (evt) {
+            this.entity = 'number';
+            this.value = evt;
+          }
+        }
+    }
 </script>
 
 <style lang="scss">
@@ -17,7 +43,7 @@
 
     .math-canvas {
         font-family: $font-family-sans;
-        font-size: 272px;
+        font-size: 72px;
         position: absolute;
         width: 100%;
         height: 100%;
---
+++ src/components/MathEntities/Number/Number.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <span class="atom" v-on:keyup="setValue" ref="input" contenteditable>{{value || ''}}</span>
+    <span>Number {{value}}</span>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Number',
+        mounted: function () {
+          this.focus();
+        },
+        props: {
+          val: Number
+        },
+        data: function () {
+            return {
+                value: Number(this.val)
+            }
+        },
+        methods:  {
+            focus: function (pos) {
+              var el = this.$refs.input;
+              var range = document.createRange();
+              var sel = window.getSelection();
+
+              range.setStart(el, String(this.value).length);
+              range.collapse(true);
+
+              sel.removeAllRanges();
+              sel.addRange(range);
+            },
+            setValue: function (evt) {
+
+              if (evt.target.innerText.length == 0) {
+                this.$emit('isempty');
+              }
+
+              this.value = Number(evt.target.innerText);
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+  .atom {
+    padding: 10px;
+    border: 1px solid green;
+
+    &::after {
+      content: "";
+      display: inline-block;
+      width: 0.1em;
+    }
+  }
+</style>
