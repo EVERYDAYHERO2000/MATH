23c8ab534bfeda013d892e7599c993e053e65642
Ilya Aralin <aralinilya@yandex-team.ru>
2020-11-20 16:37:38 +0300

    fix

--- src/App/MathUi/SidebarTools/SidebarTools.vue
+++ src/App/MathUi/SidebarTools/SidebarTools.vue
@@ -1,78 +1,62 @@
 <template>
-    
-    <Sidebar 
-        class="sidebar-tools"
-        v-bind:visible="isVisible"
-        attachment="right">
+  <Sidebar class="sidebar-tools" v-bind:visible="isVisible" attachment="right">
+    <template v-slot:header>
+      <ButtonIcon v-on:click="toggleVisible" icon="search" title="Elements" />
+      <InputSearch v-on:updateSearch="searchValue = $event" v-bind:value="searchValue" />
+    </template>
 
-        <template v-slot:header>
-            <ButtonIcon 
-                v-on:click="toggleVisible" 
-                icon="search" 
-                title="Elements" />    
-            <InputSearch 
-                v-on:updateSearch="searchValue = $event"
-                v-bind:value="searchValue" />  
-        </template>
-
-        <template v-slot:body>
-            <SidebarToolsEntities 
-                v-on:updateScroll="scroll = $event"
-                v-on:changeTab="activeTab = $event" 
-                v-bind:scrollTop="scroll" 
-                v-bind:activeTab="activeTab"
-                v-bind:searchValue="searchValue"
-                v-if="isVisible" />
-        </template>
-
-    </Sidebar>
-
+    <template v-slot:body>
+      <SidebarToolsEntities
+        v-on:updateScroll="scroll = $event"
+        v-on:changeTab="activeTab = $event"
+        v-bind:scrollTop="scroll"
+        v-bind:activeTab="activeTab"
+        v-bind:searchValue="searchValue"
+        v-if="isVisible"
+      />
+    </template>
+  </Sidebar>
 </template>
 
 <script>
-    import Sidebar from '/@components/Sidebar/Sidebar.vue';
-    import ButtonIcon from '/@components/ButtonIcon/ButtonIcon.vue';
-    import SidebarToolsEntities from './Entities/Entities.vue';
-    import InputSearch from '/@components/InputSearch/InputSearch.vue';
+import Sidebar from '/@components/Sidebar/Sidebar.vue';
+import ButtonIcon from '/@components/ButtonIcon/ButtonIcon.vue';
+import SidebarToolsEntities from './Entities/Entities.vue';
+import InputSearch from '/@components/InputSearch/InputSearch.vue';
 
-    export default {
-        name: 'SidebarTools',
-        components: {
-            Sidebar,
-            ButtonIcon,
-            SidebarToolsEntities,
-            InputSearch
-        },
-        data: function() {
-            return {
-                isVisible: false,
-                activeTab: 'math',
-                searchValue: '',
-                scroll: 0
-            }    
-        },
-        props: {
-            
-        },
-        mounted: function(){
-            
-        },  
-        methods: {
-            toggleVisible: function() {
-                this.isVisible = !this.isVisible;
-            }
-        }    
-    }    
+export default {
+  name: 'SidebarTools',
+  components: {
+    Sidebar,
+    ButtonIcon,
+    SidebarToolsEntities,
+    InputSearch,
+  },
+  data: function () {
+    return {
+      isVisible: false,
+      activeTab: 'math',
+      searchValue: '',
+      scroll: 0,
+    };
+  },
+  props: {},
+  mounted: function () {},
+  methods: {
+    toggleVisible: function () {
+      this.isVisible = !this.isVisible;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-    @import '../../../css/main.scss';
+@import '../../../css/main.scss';
 
-    .sidebar-tools {
-        .input-search {
-            flex-grow: 1;
-            margin-left: $grid-1;
-        }
-    }
-
+.sidebar-tools {
+  .input-search {
+    flex-grow: 1;
+    margin-left: $grid-1;
+  }
+}
 </style>
