c4a38ebe2c745713902bb983ee2d7c3684428ede
Ilya Aralin <aralinilya@yandex-team.ru>
2020-12-01 21:10:20 +0300

    fix pivot

--- src/App/MathCanvas/Pivot/Pivot.vue
+++ src/App/MathCanvas/Pivot/Pivot.vue
@@ -32,6 +32,9 @@ export default {
       startY: null,
     };
   },
+  mounted: function () {
+    this.renderMarkers();
+  },
   methods: {
     render: function () {
       window.requestAnimationFrame(() => {
@@ -39,6 +42,8 @@ export default {
           this.rotation
         }deg) scale(${this.scale > 0 ? this.scale : 0})`;
         this.$refs.frame.style.transform = val;
+
+        this.renderMarkers();
       });
     },
     onWheel: function (event) {
@@ -73,6 +78,107 @@ export default {
     onGestureend: function (event) {
       event.preventDefault();
     },
+    renderMarkers: function () {
+      let markerTop = this.$refs.marker_top,
+        markerBottom = this.$refs.marker_bottom,
+        markerLeft = this.$refs.marker_left,
+        markerRight = this.$refs.marker_right,
+        frame = this.$refs.frame,
+        frameRect = this.$refs.frame.getBoundingClientRect(),
+        windowHeight = window.innerHeight,
+        windowWidth = window.innerWidth,
+        padding = 200,
+        markerWidth = 3;
+
+      if (frameRect.top < 0) {
+
+        if (frameRect.left + frameRect.width > 0) {
+          markerTop.style.width = (frameRect.width) ? frameRect.width + 'px' : 80 + 'px' ;
+          markerTop.style.left = frameRect.left + 'px';
+          markerTop.style.right = 'auto'; 
+        } else {
+          markerTop.style.width = markerWidth * 2 +'px';
+          markerTop.style.left = 0;
+          markerTop.style.right = 'auto';  
+        }
+
+        if (frameRect.left > windowWidth) {
+          markerTop.style.width = markerWidth * 2 + 'px';
+          markerTop.style.left = 'auto';
+          markerTop.style.right = 0; 
+        }          
+
+      } else {
+        markerTop.style.width = 0;
+        markerTop.style.left = 0;
+      }
+
+      if (frameRect.bottom > windowHeight) {
+        if (frameRect.left + frameRect.width > 0) {
+          markerBottom.style.width = (frameRect.width > 80) ? frameRect.width + 'px' : 80 + 'px';
+          markerBottom.style.left = frameRect.left + 'px';
+          markerBottom.style.right = 'auto'; 
+        } else {
+          markerBottom.style.width = markerWidth * 2 +'px';
+          markerBottom.style.left = 0;
+          markerBottom.style.right = 'auto';  
+        }
+
+        if (frameRect.left > windowWidth) {
+          markerBottom.style.width = markerWidth * 2 + 'px';
+          markerBottom.style.left = 'auto';
+          markerBottom.style.right = 0; 
+        }
+      } else {
+        markerBottom.style.width = 0;
+        markerBottom.style.left = 0;
+      }
+
+      if (frameRect.left < 0) {
+        if (frameRect.top + frameRect.height > 0) {
+          markerLeft.style.height = (frameRect.height > 80) ? frameRect.height + 'px' : 80 + 'px';
+          markerLeft.style.top = frameRect.top + 'px';
+          markerLeft.style.bottom = 'auto';
+        } else {
+          markerLeft.style.height = markerWidth * 2 + 'px';
+          markerLeft.style.top = 0;
+          markerLeft.style.bottom = 'auto';
+
+        }
+
+        if (frameRect.top > windowHeight) {
+          markerLeft.style.height = markerWidth * 2 + 'px';
+          markerLeft.style.top = 'auto';
+          markerLeft.style.bottom = 0;
+        }
+
+      } else {
+        markerLeft.style.height = 0;
+        markerLeft.style.top = 0;
+      }
+
+      if (frameRect.right > windowWidth) {
+        if (frameRect.top + frameRect.height > 0) {
+          markerRight.style.height = (frameRect.height) ? frameRect.height + 'px' : 80 + 'px';
+          markerRight.style.top = frameRect.top + 'px';
+          markerRight.style.bottom = 'auto';
+        } else {
+          markerRight.style.height = markerWidth * 2 + 'px';
+          markerRight.style.top = 0;
+          markerRight.style.bottom = 'auto';
+
+        }
+
+        if (frameRect.top > windowHeight) {
+          markerRight.style.height = markerWidth * 2 + 'px';
+          markerRight.style.top = 'auto';
+          markerRight.style.bottom = 0;
+        }
+      } else {
+        markerRight.style.height = 0;
+        markerRight.style.top = 0;
+      }
+    },
   },
 };
 </script>
@@ -80,15 +186,20 @@ export default {
 <style lang="scss">
 @import '../../../css/main.scss';
 
+$width-marker: 3px;
+
 .math-canvas {
   &__pivot {
     width: 10000px;
     height: 10000px;
+    box-sizing: border-box;
+    padding: 200px;
   }
   &__frame {
     transform-origin: center;
     position: absolute;
-    padding: 200px;
+    padding: $grid-3;
+    box-sizing: border-box;
   }
 
   &__marker {
@@ -96,6 +207,7 @@ export default {
     display: block;
     pointer-events: none;
     touch-action: none;
+    z-index: 10000;
 
     .app_theme_light & {
       background-color: rgba($color-primary_theme_light, 1);
@@ -106,19 +218,19 @@ export default {
     }
 
     &_top {
-      height: 1px;
+      height: $width-marker;
       top: 0;
     }
     &_bottom {
-      height: 1px;
+      height: $width-marker;
       bottom: 0;
     }
     &_left {
-      width: 1px;
+      width: $width-marker;
       left: 0;
     }
     &_right {
-      width: 1px;
+      width: $width-marker;
       right: 0;
     }
   }
